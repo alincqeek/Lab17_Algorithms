a32d688f9196648c8d7a5aa344196f8c78efe206
alincqeek <a72658695@gmail.com>
2026-02-25 18:08:36 +0300

    Step 8: functions and modular programming

--- code/step8_functionDiagram/Program.cs
+++ code/step8_functionDiagram/Program.cs
@@ -22,4 +22,22 @@ Console.Write("Введите 2 числа: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите номер операции 1-сумма; 2-умножение; 3-деление: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number3=1) { }
+if (number3 == 1) 
+{ 
+    int result = Sum(number1, number2);
+    Console.WriteLine($"Результат: {result}");
+}
+else if (number3 == 2)
+{
+    int result = Mul(number1, number2);
+    Console.WriteLine($"Результат: {result}");
+}
+else if (number3 == 3)
+{
+    double  result = Dele(number1, number2);
+    Console.WriteLine($"Результат: {result}");
+}
+else
+{
+    Console.WriteLine("Неверный номер операции!");
+}
