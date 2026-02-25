de50a6d3c6a0960c62a63eed129cc70b0a68ef47
Alina <a72658695@gmail.com>
2026-02-25 11:04:49 +0300

    Step 7 arrays

---
+++ code/step7_arrayMax/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+int[] grade = { 5, 4, 5, 3, 4 };
+int s = 0;
+for (int i = 0; i <grade.Length ; i++)
+{
+    if (s<grade[i]) { s = grade[i]; }
+};
+Console.WriteLine(s);
